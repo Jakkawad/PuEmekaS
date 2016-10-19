a0c0f91342b8c6092dd3578cb867eb823d1d9c6e
Jakkawad <admin@all2sale-imac.local>
2016-10-19 12:47:38 +0700

    Initial Commit

---
+++ WakeMeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WakeMeUp
+//
+//  Created by admin on 10/19/2559 BE.
+//  Copyright © 2559 Jakkawad.Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
