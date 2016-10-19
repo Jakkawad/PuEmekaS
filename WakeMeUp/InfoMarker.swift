c0c1e6fc6a5c1a8be51da3433e19e64be6631a95
Jakkawad <admin@all2sale-iMac.local>
2016-10-19 16:17:16 +0700

    Add notification
    notification

---
+++ WakeMeUp/InfoMarker.swift
@@ -0,0 +1,22 @@
+//
+//  InfoMarker.swift
+//  WakeMeUp
+//
+//  Created by admin on 10/19/2559 BE.
+//  Copyright © 2559 Jakkawad.Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class InfoMarker: UIView {
+
+    @IBOutlet weak var lblName: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- WakeMeUp/ViewController.swift
+++ WakeMeUp/ViewController.swift
@@ -7,11 +7,83 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView:GMSMapView!
+    
+    //Location Manager delegates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:14)
+        mapView.animate(to: camera)
+        
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
+    }
+    
+//    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+//        print("didTapmarker")
+//        return true
+//    }
+
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let infoWindow = Bundle.main.loadNibNamed("InfoMarker", owner: self, options: nil)?.first! as! InfoMarker
+        infoWindow.lblName.text = "\(marker.position.latitude) \(marker.position.longitude)"
+        return infoWindow
+    }
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        print("didTapIndoWindowOfmarker")
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        print("Mark: \(coordinate.latitude), \(coordinate.longitude)")
+        print("didTapAtcoordinate")
+        let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude))
+        marker.title = "Mark1"
+        marker.map = mapView
+    }
+    
+//    func mapViewDidStartTileRendering(_ mapView: GMSMapView) {
+//        print("didStartTileRendering")
+//    }
+//    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+////        let location = mapView.myLocation
+////        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:14)
+////        mapView.animate(to: camera)
+////        let yourCurrentLocation = mapView.myLocation
+////        print("youCurrentLocation: \(yourCurrentLocation)")
+//        print("didUpdateHeading")
+//    }
+    func mapViewDidFinishTileRendering(_ mapView: GMSMapView) {
+        let location = mapView.myLocation
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:14)
+        mapView.animate(to: camera)
+        let yourCurrentLocation = mapView.myLocation
+        print("youCurrentLocation: \(yourCurrentLocation)")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // MARK: MapView
+        self.mapView.delegate = self
+        self.mapView.settings.myLocationButton = true
+        self.mapView.isMyLocationEnabled = true
+        self.mapView.settings.compassButton = true
+        
+        // MARK: Location
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
